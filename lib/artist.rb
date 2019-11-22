77666ed080ab8aa19c4a45c92f42da82466edc1b
Kizito Njoku <nkizito57@gmail.com>
2019-11-22 17:49:34 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+require "pry"
+class Artist 
+  attr_accessor :name, :genre, :songs
+  @@all = []
+  def initialize(name)
+    @name = name 
+    @genre = genre
+    @songs = songs
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+  
+  def genres 
+     Song.all.collect {|g| g.genre}
+  end
+  
+  def songs 
+    Song.all.select {|song| song.artist == self }
+  end 
+  
+  
+end 
---
+++ lib/genre.rb
@@ -0,0 +1,21 @@
+class Genre 
+  attr_accessor :name 
+  @@all = []
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end 
+  
+  def songs 
+    Song.all.collect {|song| song}
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def artists 
+    Song.all.collect {|song| song.artist }
+  end 
+  
+end 
