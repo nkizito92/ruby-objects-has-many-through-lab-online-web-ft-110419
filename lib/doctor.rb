aa941de30e287bac8c5a40a9f5e7222c47da78c3
Kizito Njoku <nkizito57@gmail.com>
2019-11-22 19:32:01 +0000

    Done.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,8 +1,26 @@
 class Doctor 
-    attr_accessor :name 
-    
+    attr_accessor :name, :appointments, :patients
+    @@all = []
     def initialize(name)
       @name = name 
-      
+      @appointments = appointments
+      @patients = patients
+      @@all << self
+    end 
+    
+    def self.all 
+      @@all 
+    end
+    
+    def new_appointment(lvl, date)
+      Appointment.new(date, lvl, self)
+    end 
+    
+    def appointments 
+      Appointment.all.select {|apponint| apponint.doctor == self}
+    end 
+    
+    def patients 
+      appointments.collect {|app| app.patient }
     end 
 end  
